3e3728cfe6853589476d9af765a730036eff3896
Vivek Raghav <93561365+vivekrajraghav@users.noreply.github.com>
2022-09-03 11:54:32 +0530

    upload

---
+++ views/contact.js
@@ -0,0 +1,9 @@
+function message(){
+    var name = document.getElementById('name');
+    var phone = document.getElementById('phone');
+    var email = document.getElementById('email');
+    var location = document.getElementById('location');
+    const send = document.getElementById('send');
+    const danger = document.getElementById('danger');
+}
+    
