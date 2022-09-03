955aee99fdc5bae04168d288a2a893453eca77ba
Vivek Raghav <93561365+vivekrajraghav@users.noreply.github.com>
2022-09-03 12:35:39 +0530

    yes

--- views/contact.js
+++
@@ -1,9 +0,0 @@
-function message(){
-    var name = document.getElementById('name');
-    var phone = document.getElementById('phone');
-    var email = document.getElementById('email');
-    var location = document.getElementById('location');
-    const send = document.getElementById('send');
-    const danger = document.getElementById('danger');
-}
-    
